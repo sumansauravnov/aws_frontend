663c3b6ddda7a8d856ddbb2b3ae852ec47a752e1
sumansauravnov <suman.saurav@recqarz.com>
2025-03-22 21:51:36 +0530

    tt

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    host: "0.0.0.0", // Allows access from any IP
+    port: 3001, // Runs on port 3001
+  },
 })
